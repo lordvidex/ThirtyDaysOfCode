1ae1d7bc070daa7948740027a4512e27d67ae16d
lordvidex <lordvidex.prime@gmail.com>
2020-03-30 15:42:56 +0300

    Day 5 track 1 -fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
-void countTheWord(String s){
-  Map<String,int>answer = {};
-  //first replace all space related symbols or unicodes to ' '
-  //for uniformity
-  var myString = s.toLowerCase().replaceAll(new RegExp(r'\\t|\\n'),' ');
-  //Regular expression that matches all required words and adds them to  map
-  var sh = RegExp(r"(\w+'\w+)|(\w+)").
-    allMatches(myString).map((m)=>m[0]).forEach((f){
-    answer[f]==null?
-      answer[f]=1:
-    answer[f]+=1;
-  });
-  
-  //Output
-  answer.forEach((k,v)=>print('$k: $v'));
-  
+import 'dart:math';
+String differenceOfSquares(int n){
+  //absolute difference of squares of sum  &&&   sum of squares
+  return (pow((n*(n+1))/2,2)-((n*(n+1)*(2*n+1))/6)).abs().toStringAsFixed(0);
 }
 void main() {
-  var s = "\"That's the password: 'PASSWORD 123'!\", cried the Special Agent.\nSo I fled.";
-  countTheWord(s);
-  
+  int n = 10;
+  print(differenceOfSquares(n));  
 }
