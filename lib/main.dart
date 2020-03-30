5f7c36c837d7c58b5e472a237e16ae68e1f6ac32
lordvidex <lordvidex.prime@gmail.com>
2020-03-30 15:22:35 +0300

    Day 4 Track 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
-Map<String,bool> count = {};
-bool isIsogram(String s) {
-  var answer = true;
-  s = s.replaceAll(new RegExp(r'\s|-'), '').toLowerCase();
-  s.split('').forEach((sd){
-    if(count[sd]==null)count[sd]= true;
-    else answer = false;
+void countTheWord(String s){
+  Map<String,int>answer = {};
+  //first replace all space related symbols or unicodes to ' '
+  //for uniformity
+  var myString = s.toLowerCase().replaceAll(new RegExp(r'\\t|\\n'),' ');
+  //Regular expression that matches all required words and adds them to  map
+  var sh = RegExp(r"(\w+'\w+)|(\w+)").
+    allMatches(myString).map((m)=>m[0]).forEach((f){
+    answer[f]==null?
+      answer[f]=1:
+    answer[f]+=1;
   });
-  return answer;
+  
+  //Output
+  answer.forEach((k,v)=>print('$k: $v'));
+  
 }
-
 void main() {
-  print(isIsogram('downstream')?'String is an Isogram':'String is NOT an Isogram');
+  var s = "\"That's the password: 'PASSWORD 123'!\", cried the Special Agent.\nSo I fled.";
+  countTheWord(s);
+  
 }
