dc5264793b4bea52b0a4e94751f8943c49bc9d75
lordvidex <lordvidex.prime@gmail.com>
2020-03-30 17:07:52 +0300

    Day 6 track 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'dart:math';
-String differenceOfSquares(int n){
-  //absolute difference of squares of sum  &&&   sum of squares
-  return (pow((n*(n+1))/2,2)-((n*(n+1)*(2*n+1))/6)).abs().toStringAsFixed(0);
+String findPrimeFactors(int n) {
+  assert(!n.isNegative && n.isFinite); //just some extra input validations
+  List<num> ans = [];
+  while (n % 2 == 0) {
+    n ~/= 2;
+    ans.add(2);
+  }
+  for (var x = 3; x <= sqrt(n); x += 2)
+    while (n % x == 0) {
+      n ~/= x;
+      ans.add(x);
+    }
+  if (n > 2) ans.add(n);
+  return ans.join(', ');
 }
+
 void main() {
-  int n = 10;
-  print(differenceOfSquares(n));  
+  int n = 10000;
+  print(findPrimeFactors(n));
 }
