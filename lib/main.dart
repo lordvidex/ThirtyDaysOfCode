cc31ff05ecdeb5c3d75e6515c8ce24f5e5338984
lordvidex <lordvidex.prime@gmail.com>
2020-03-31 15:42:14 +0300

    Day 7 Track 1:: Pascal Triangle

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,34 @@
 import 'dart:math';
-String findPrimeFactors(int n) {
-  assert(!n.isNegative && n.isFinite); //just some extra input validations
-  List<num> ans = [];
-  while (n % 2 == 0) {
-    n ~/= 2;
-    ans.add(2);
+
+int combination(int n, int r) {
+  if (n == 0 || r == 0) {
+    return 1;
   }
-  for (var x = 3; x <= sqrt(n); x += 2)
-    while (n % x == 0) {
-      n ~/= x;
-      ans.add(x);
+  int x = 1; //numerator 
+  int d = 1; //denominator
+  for (int i = max(r, n - r) + 1, j = 1; i <= n; i++, j++) {
+    x *= i;
+    d *= j;
+  }
+  x ~/= d;
+  return x;
+}
+
+void pascalTriangle(int numberOfRows) {
+  //Instead of looping and recreating arrays.. we can simply use Combinations to
+  //generate the fields in each row
+  for (int i = 0; i < numberOfRows; i++) {
+    List<int> arr = [];
+    int count = i;
+    while (count >= 0) {
+      arr.add(combination(i, count));
+      count--;
     }
-  if (n > 2) ans.add(n);
-  return ans.join(', ');
+    print(arr.join(' '));
+  }
 }
 
 void main() {
-  int n = 10000;
-  print(findPrimeFactors(n));
+  int numberOfRows = 10;
+  pascalTriangle(numberOfRows);
 }
