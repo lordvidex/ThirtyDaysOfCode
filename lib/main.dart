12e7ad67962468961db2fd176bcf1aed95d47e4e
lordvidex <lordvidex.prime@gmail.com>
2020-04-02 14:51:39 +0300

    Day 9 Track 1:: Solved this in codingame.com

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,14 @@
-//Decided to go OOP like Dart should be.. D:)
-class Triangle{
-  final num _lengthA;
-  final num _lengthB;
-  final num _lengthC;
-  Triangle(this._lengthA,this._lengthB,this._lengthC);
-  String get triangleType{
-    List<String> _result = [];
-    if(_lengthA==_lengthB||_lengthB==_lengthC||_lengthA==_lengthC){
-      _result.add('Isosceles');
-      if(_lengthA==_lengthB&&_lengthA==_lengthC){
-        _result.add('Equilateral');
-      }
-    }else{
-      _result.add('Scalene');
+bool isPanagram(String s){
+  String all = 'abcdefghijklmnopqrstuvwxyz';
+  var new_s = s.replaceAll(' ','').toLowerCase();
+  for(var i = 0;i<all.length;i++){
+    if(!new_s.contains(all[i])){
+      return false;
     }
-    return _result.join(' and ')+' Triangle';
   }
+  return true;
 }
-
 void main(){
-  Triangle myFirstTriangle = Triangle(20,20,20);
-  Triangle mySecondTriangle = Triangle(20,20,30);
-  Triangle myThirdTriangle = Triangle(10,13,14);
-  print(myFirstTriangle.triangleType);
-  print(mySecondTriangle.triangleType);
-  print(myThirdTriangle.triangleType);
+  String s = 'The quick brown fox jumps over the lazy dog';
+  print(isPanagram(s));
 }
