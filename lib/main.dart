a313f0bb1ac1bda181b0280d5030474546d166a6
lordvidex <lordvidex.prime@gmail.com>
2020-04-03 20:29:55 +0300

    Day 10 Track 1 task:: Using BigInt

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
-bool isPanagram(String s){
-  String all = 'abcdefghijklmnopqrstuvwxyz';
-  var new_s = s.replaceAll(' ','').toLowerCase();
-  for(var i = 0;i<all.length;i++){
-    if(!new_s.contains(all[i])){
-      return false;
-    }
-  }
-  return true;
+void printGrain(int squarePos){
+  
+  /// Accuracy is [Maximal]
+  ///by using BigInt in case results are 
+  /// processed based on 32 bits int
+  ///Possible max value 2^64-1
+
+  //grains on the nth square position is 2^(n-1)
+  print('Square $squarePos: ${BigInt.two.pow(squarePos-1)} grains');
+  
+  //Using Sum of GP a*(r^n-1)/r-1=>r^n-1 where r=2;n=64
+  print('Total number of grains: ${BigInt.two.pow(64)-BigInt.one}');
 }
 void main(){
-  String s = 'The quick brown fox jumps over the lazy dog';
-  print(isPanagram(s));
+  var square = 64;
+  printGrain(square);
 }
